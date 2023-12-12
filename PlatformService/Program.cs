5e22a67f7cfc7ec902b54d5ca41a4608d14b26b1
Keith <kayceecode@gmail.com>
2023-12-11 19:09:20 -0500

    Added TrusterServerCerficate to appsettings

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -8,29 +8,27 @@ var configuration = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
-
 Console.WriteLine($"--> Command Service Endpoint {configuration["CommandService"]}");
 
-if (builder.Environment.IsProduction())
-{
+// if (builder.Environment.IsProduction())
+// {
     builder.Services.AddDbContext<AppDbContext>(     
         opt => opt.UseSqlServer(configuration.GetConnectionString("PlatformsConn")));
-    builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
     Console.WriteLine("--> Using Sql Server Db");
-}
-else
-{
-    Console.WriteLine("--> Using In-Memory Db");
-    builder.Services.AddDbContext<AppDbContext>(opt =>
-        opt.UseInMemoryDatabase("InMem"));
-}
+// }
+// else
+// {
+//     Console.WriteLine("--> Using In-Memory Db");
+//     builder.Services.AddDbContext<AppDbContext>(opt =>
+//         opt.UseInMemoryDatabase("InMem"));
+// }
 
 var app = builder.Build();
 
